bb73568376c8174370ad6ca9d6324516cba10718
Rohit Sharma <rohitrsh@gmail.com>
2020-04-13 00:37:34 +0100

    adding cluster status job

--- hokstack/files/datanode/data-pre.sh
+++ hokstack/files/datanode/data-pre.sh
@@ -10,3 +10,17 @@ echo $PODNAME > /etc/hostname
 echo "AllowUsers *@10.42.*.*" >> sshd_config
 
 curl -u hokstack:h0kStack -X POST -H 'X-Requested-By:ambari' -d'{"RequestInfo":{"context":"Updating /etc/hosts file", "action" : "update_hosts", "service_name" : "", "component_name":"", "hosts":"ambariserver-0"}}' http://$AMBARISERVER:8080/api/v1/clusters/$NAMESPACE/requests
+
+# HOST=$PODNAME.$NAMESPACE
+# STATUS=$(curl --silent --user hokstack:h0kStack http://$AMBARISERVER:8080/api/v1/clusters/$NAMESPACE/hosts?fields=Hosts/host_state,Hosts/host_status |jq -r --arg HOST "$HOST" '.items[] | select(.Hosts.host_name==$HOST) | [.Hosts.host_state] | .[]')
+
+# while ([ -z $STATUS ] || [ $STATUS != HEALTHY ]); do
+#   echo "Waiting for $HOST status to become HEALTHY!"
+#   echo $STATUS
+#   sleep 5
+# done
+
+# if ([ -z $STATUS ] || [ $STATUS == HEALTHY ]); then
+#   echo "Server is $STATUS"
+#   cmd=$(curl -u hokstack:h0kStack -X POST -H 'X-Requested-By:ambari' -d'{"RequestInfo":{"context":"Updating /etc/hosts file", "action" : "update_hosts", "service_name" : "", "component_name":"", "hosts":"ambariserver-0"}}' http://$AMBARISERVER:8080/api/v1/clusters/$NAMESPACE/requests)
+# fi
